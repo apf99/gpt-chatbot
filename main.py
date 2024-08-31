103cb1f14764d9a3253ac427f622ca459ab8e5fd
Andrew Friedman <andrew@cyberdreaming.net>
2024-08-31 10:18:25 -0700

    Connect the front end to the backend.  Add threading to the response.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from PyQt6.QtWidgets import QMainWindow, QTextEdit, QPushButton, QApplication
-
+from PyQt6.QtWidgets import QMainWindow, QTextEdit, QPushButton, QApplication, QLineEdit
+from backend import Chatbot
 import sys
+import threading
 
 class ChatbotWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.chatbot = Chatbot()
 
         self.setMinimumSize(700, 500)
 
@@ -14,14 +17,28 @@ class ChatbotWindow(QMainWindow):
         self.chat_area.setReadOnly(True)
 
         # Add the input field widget
-        self.input_field = QTextEdit(self)
+        self.input_field = QLineEdit(self)
         self.input_field.setGeometry(10, 340, 480, 40)
+        self.input_field.returnPressed.connect(self.send_message)
 
         # Add a button
         self.button = QPushButton('Send', self)
         self.button.setGeometry(500, 340, 100, 40)
+        self.button.clicked.connect(self.send_message)
 
         self.show()
+
+    def send_message(self):
+        user_input = self.input_field.text().strip()
+        self.chat_area.append(f'<p stype="color:#333333">Me: {user_input}</p>')
+        self.input_field.clear()
+
+        thread = threading.Thread(target=self.get_bot_response, args=(user_input, ))
+        thread.start()
+
+    def get_bot_response(self, user_input):
+        response = self.chatbot.get_response(user_input)
+        self.chat_area.append(f'<p stype="color:#333333; background-color: #E9E9E9">Bot: {response}</p>')
 
 
 app = QApplication(sys.argv)
